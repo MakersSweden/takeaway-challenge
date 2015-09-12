d5c832a9cf1f177872f78c943e729013c29433cd
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-09-12 18:10:08 +0200

    Added method for calaculate order total plus tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,8 +5,15 @@ class Customer
     @order = []
   end
 
-  def add_to_order(dish)
-    self.order << (dish)
+  def add_to_order(nr, dish)
+    (nr).times do
+      self.order << (dish)
+    end
+  end
+
+  def order_total
+      sum = self.order.map(&:price).inject(0, &:+)
+      sum
   end
 
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -21,20 +21,29 @@ describe 'order' do
   subject {Customer.new}
   let(:restaurant) { Restaurant.new }
 
-  it 'adds an item to order' do
-    subject.add_to_order(restaurant.menu[:nr1])
+  it 'can add a dish to order sheet' do
+    subject.add_to_order(1, restaurant.menu[:nr1])
     expect(subject.order).not_to be_empty
   end
 
-    it 'order item has a name' do
-    subject.add_to_order(restaurant.menu[:nr1])
+    it 'can add a dish in higher quantities than 1 in order sheet' do
+    subject.add_to_order(2, restaurant.menu[:nr1])
+    expect(subject.order[1]).not_to be_nil
+  end
+
+    it 'dish in order sheet has a name' do
+    subject.add_to_order(1, restaurant.menu[:nr1])
     expect(subject.order[0].name).to eq "Fried rice"
   end
 
-    it 'order item has a price' do
-    subject.add_to_order(restaurant.menu[:nr1])
+    it 'dish in order sheet has a a price' do
+    subject.add_to_order(1, restaurant.menu[:nr1])
     expect(subject.order[0].price).to eq 65
   end
 
+    it 'can calculate the total' do
+    subject.add_to_order(2, restaurant.menu[:nr1])
+    expect(subject.order_total).to eq 130
+  end
 
 end
