85c10751ee02d4f21ac863b8d30573b8570e521f
Christopher Comella <git.chrisco@gmail.com>
2015-09-12 21:14:43 +0200

    Add cost functionality to OrderItem and tests (cost = p * q)

--- lib/order_item.rb
+++ lib/order_item.rb
@@ -3,11 +3,15 @@ class OrderItem
   attr_accessor :name
   attr_accessor :unit_price
   attr_accessor :quantity
+  attr_accessor :cost
+  attr_accessor :currency
 
   def initialize(menu, item_number, quantity)
     @menu_item = menu.todays_menu[item_number]
     @name = menu_item.name
     @unit_price = menu_item.unit_price
+    @currency = menu_item.currency
     @quantity = quantity
+    @cost = @unit_price * quantity
   end
 end
--- spec/order_item_spec.rb
+++ spec/order_item_spec.rb
@@ -15,27 +15,47 @@ describe OrderItem do
     expect(order_item_1).to be_a_kind_of(OrderItem)
   end
 
-  it "the first item ordered is a 'Burger'" do
-    expect(order_item_1.name).to eq("Burger")
+  context "one 'Burger' is ordered" do
+    it "the item ordered is 'Burger'" do
+      expect(order_item_1.name).to eq("Burger")
+    end
+
+    it "the Burger costs '100'" do
+      expect(order_item_1.unit_price).to eq(100)
+    end
+
+    it "the number of Burgers ordered is '1'" do
+      expect(order_item_1.quantity).to eq(1)
+    end
+
+    it "the total cost of the OrderItem is '100'" do
+      expect(order_item_1.cost).to eq(100)
+    end
+
+    it "the currency is 'USD'" do
+      expect(order_item_1.currency).to eq("USD")
+    end
   end
 
-  it "the Burger costs '100'" do
-    expect(order_item_1.unit_price).to eq(100)
-  end
+  context "two orders of 'Fries' are ordered" do
+    it "the item ordered is 'Fries'" do
+      expect(order_item_2.name).to eq("Fries")
+    end
 
-  it "the number of Burgers ordered is '1'" do
-    expect(order_item_1.quantity).to eq(1)
-  end
+    it "each order of Fries costs '100'" do
+      expect(order_item_2.unit_price).to eq(50)
+    end
 
-    it "the second item ordered is an order of 'Fries'" do
-    expect(order_item_2.name).to eq("Fries")
-  end
+    it "the number of orders of Fries ordered is '2'" do
+      expect(order_item_2.quantity).to eq(2)
+    end
 
-  it "the Fries cost '100'" do
-    expect(order_item_2.unit_price).to eq(50)
-  end
+    it "the total cost of the OrderItem is '100'" do
+      expect(order_item_1.cost).to eq(100)
+    end
 
-  it "the number of orders of Fries ordered is '2'" do
-    expect(order_item_2.quantity).to eq(2)
+    it "the currency is 'USD'" do
+      expect(order_item_1.currency).to eq("USD")
+    end
   end
 end
