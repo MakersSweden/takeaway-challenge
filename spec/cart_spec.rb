e0a5b13d1f8561ca1a7fdb5d28f22d7b80136e33
Christopher Comella <git.chrisco@gmail.com>
2015-09-13 13:33:53 +0200

    Create restore point before I toss and rebuild my Cart singleton class

---
+++ spec/cart_spec.rb
@@ -0,0 +1,21 @@
+require "cart"
+
+describe Cart do
+
+  # TODO: Learn how to use doubles/specs/mocks instead of this:
+  let(:menu_item_1) { MenuItem.new(1, "Burger", "The best burger in the world", 100, "USD") }
+  let(:menu_item_2) { MenuItem.new(2, "Fries", "The best fries in the world", 50, "USD") }
+  let(:menu) { Menu.new([menu_item_1, menu_item_2]) }
+  let(:order_item_1) { OrderItem.new(menu, 0, 1) }
+  let(:order_item_2) { OrderItem.new(menu, 1, 2) }
+  let(:cart) { Cart.send(:new) }
+
+  it "is a kind of Cart" do
+    expect(cart).to be_a_kind_of(Cart)
+  end
+
+  xit "adds a 'Burger' to the cart" do
+    expect(cart.load_item(order_item_1)).to # Add the Burger
+  end
+
+end
